23d44d19bb64ad29951dab3fbe216bcc3b0e331a
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-10 10:49:06 +0100

    singleton: red

--- src/designs/singleton/singleton.spec.ts
+++ src/designs/singleton/singleton.spec.ts
@@ -4,4 +4,10 @@ describe('Singleton class "Logger"', () => {
 	it("may have regular props", () => {
 		expect(Logger.getProp).toBeDefined;
 	});
+
+	it("must have only 1 instance", () => {
+		const s1 = Logger.getInstance;
+		const s2 = Logger.getInstance;
+		expect(s1).toEqual(s2);
+	});
 });
