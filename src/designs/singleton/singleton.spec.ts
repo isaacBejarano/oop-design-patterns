b0724f495669c5e4342af55bdde6fd709356daac
isaacBejarano <isaacbejaranopro@gmail.com>
2021-11-10 16:48:59 +0100

    singleton: refactor

--- src/designs/singleton/singleton.spec.ts
+++ src/designs/singleton/singleton.spec.ts
@@ -1,19 +1,22 @@
 import Logger from "./singleton";
 
+let s1: Logger, s2: Logger;
+
+beforeEach(() => {
+	s1 = Logger.getInstance();
+	s2 = Logger.getInstance();
+});
+
 describe('Singleton class "Logger"', () => {
 	it("may have regular props", () => {
 		expect(Logger.getProp).toBeDefined;
 	});
 
 	it("must have only 1 instance", () => {
-		let s1 = Logger.getInstance();
-		let s2 = Logger.getInstance();
 		expect<Logger>(s1).toBe<Logger>(s2);
 	});
 
 	it("is responsible of defining that instance", () => {
-		let s1 = Logger.getInstance();
-		// let s2 = Logger.getInstance();
 		expect<Logger>(s1).not.toBe(undefined);
 	});
 });
